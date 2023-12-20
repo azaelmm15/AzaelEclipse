a59068f984ff6a803c543768bdaf24dd8ff7e7a9
azaelmm15 <153625953+azaelmm15@users.noreply.github.com>
2023-12-20 15:50:59 +0100

    Update Principal.java

--- Proyecto Prueba/src/Principal.java
+++ Proyecto Prueba/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Holi wakenobi");
 		System.out.println("Agur yogur! Azael");
+		System.out.println("Ciao Pescau! Azael");
 	}
 
 }
